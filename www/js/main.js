aab9f1bf26bbf464f59e8c2be974f026e492e4a7
Putu Wirya <tuebom.wirya@gmail.com>
2019-09-18 14:19:19 +0800

    update

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,38 @@
+var $$ = Dom7;
+
+var items   = [];
+
 var app = new Framework7({
   root: '#app',
-  name: 'My App',
-  id: 'com.myapp.test',
+  id:   'com.askitchen.stockinfo',
+  name: 'Stock Info',
+  
+  data: function () {
+    return {
+      // db: null,
+      // username: null,
+      // password: null,
+
+      endpoint: 'https://askitchen.com/api/v1/',
+
+    };
+  },
+  // App root methods
+  methods: {
+    
+    itemExists: function(kode) {
+      
+      var bFound = false;
+
+      for (var i=0; i < items.length; i++)
+        if (items[i].kdbar === kode) {
+          
+          bFound = true;
+          break;
+        }
+      return bFound;
+    },
+  },
   routes: [
     // Add your routes here
     // Example:
